f5f6a2701028201f377af2bc1440907e5a63c0c1
lifegatejustice <lifegatedetom@gmail.com>
2025-09-06 10:52:36 +0100

    Return all users in response for getAll endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,7 @@ const getAll = async (req, res) => {
 const result = await mongodb.getDatabase().db().collection('users').find();
     result.toArray().then(users => {
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(users[0]);
+        res.status(200).json(users);
     });
 };
 
